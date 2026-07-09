ba07bb3c05e787b8cf2aa53a9dc953b58e566a23
Andrew Manley <admanley21@gmail.com>
2026-07-09 20:15:54 +1000

    yelp-mvp seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+restaurants_data = [
+  { name: "Epicure", address: "75008 Paris", category: "french" },
+  { name: "Pellegrini's", address: "66 Bourke St, Melbourne", category: "italian" },
+  { name: "Chin Chin", address: "125 Flinders Ln, Melbourne", category: "chinese" },
+  { name: "Shujinko", address: "225 Russell St, Melbourne", category: "japanese" },
+  { name: "Fasta Pasta", address: "101 Main St, Mildura", category: "italian" }
+]
+
+restaurants_data.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished! Created #{Restaurant.count} restaurants."
